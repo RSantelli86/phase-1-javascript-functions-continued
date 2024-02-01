00b8f3bec129fe556ad72ce4c80e15723cad862f
Rey <rsantelli@luc.edu>
2024-01-31 20:35:32 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Defines saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Defines mondayWork function
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Defines wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
